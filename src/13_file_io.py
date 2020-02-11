4a07cba22539c83f8435e786ca361d2d3a399526
heiligbasil <basilief@yahoo.com>
2020-02-10 21:01:54 -0800

    File I/O, reading and writing

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,12 +8,24 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
+import sys
 
-# YOUR CODE HERE
+with open(sys.path[0] + '\\foo.txt') as f:
+    print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+lines = ['Spock wants to go swim\n', 'Why are all the pools empty?\n', 'No rain on Vulcan']
+with open(sys.path[0] + '\\bar.txt', 'w') as f:
+    for line in lines:
+        f.writelines(line)
+
+with open(sys.path[0] + '\\bar.txt') as f:
+    for i, line in enumerate(f):
+        if lines[i] == line:
+            print(line, end='')
+        else:
+            print("Comparison failed!")
